855f8640e8c9382a173a65cab2a2a54b2b7df658
varun kangotra <varunkangotra.it25@gmail.com>
2025-03-24 18:17:26 +0530

    JS Object Added

---
+++ Week-4/JS_Objects.js
@@ -0,0 +1,16 @@
+let obj1 ={
+    id: 12,
+    name: 'Varun',
+    role: 'ReactJS Expert'    
+}
+
+let obj2 = new Object();
+
+obj2.id = 13;
+obj2.name = 'Tanu';
+obj2.role = 'Sr. FrontEnd Developer';
+obj2.age = 45;
+
+
+let id1 = obj1['id'];
+let id2 = obj2.id;
