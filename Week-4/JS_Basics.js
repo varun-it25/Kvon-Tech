04174829e48eabbbd17dd3a8e1fa68fa69d3451b
varun kangotra <varunkangotra.it25@gmail.com>
2025-03-24 17:49:17 +0530

    JS Basics Added

---
+++ Week-4/JS_Basics.js
@@ -0,0 +1,19 @@
+var v = 1;
+
+let l = 1;
+
+const c = 1;
+
+let f = 1.0;
+
+let bi = BigInt(3454234345434545);
+
+let s1 = "Str 1";
+let s2 = 'Str 2';
+
+let b1 = true;
+let b2 = l == f;
+let b3 = l === bi;
+
+// Varun Comment 1
+/* Varun Comment 2 */
